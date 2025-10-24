21c606f5b573b809326cee62bbfa194a09c3bf01
Max Lübke <mluebke@uni-potsdam.de>
2025-10-24 09:41:25 +0200

    docs(license): add SPDX identifier to source files

--- src/Init/GridInit.cpp
+++ src/Init/GridInit.cpp
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * GridInit.cpp - Implementation of grid initialization
+ * Copyright (C) 2024-2025 Max Luebke (University of Potsdam)
+ */
+
 #include "InitialList.hpp"
 
 #include "PhreeqcMatrix.hpp"
